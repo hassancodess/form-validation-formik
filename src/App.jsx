02165773ddb74d2a1f604d2e8fb6838210b28903
hassancodess <dev.hassancodess@gmail.com>
2022-08-08 01:32:45 +0500

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+
 function App() {
-  return <div className='App'>Formik</div>
+  return (
+    <Router>
+      <div className='container max-w-7xl mx-auto p-6'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
 export default App
